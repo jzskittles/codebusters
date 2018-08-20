fb35d5c8a44c50d8db1ecdc6da3ca1ddf04cee91
Jenny <jz.skittles@gmail.com>
2018-08-20 10:17:13 -0500

    clean up unnecessary parts of debugging, vigenere and affine work

--- code busters.py
+++ code busters.py
@@ -7,8 +7,7 @@ master.configure(background="white")
 
 values = dict()
 for index, letter in enumerate(string.ascii_lowercase):
-    if index not in values:
-        values[letter] = index
+    values[letter] = index
 
 key = StringVar()
 phrase = StringVar()
@@ -20,17 +19,8 @@ keynum = []
 solnum = []
 solution = StringVar()
 solution.set("")
-cat = StringVar()
-cat.set("")
-catb = StringVar()
-catb.set("")
 
-
-
-def raise_frame(frame):
-    frame.tkraise()
-
-def mono():
+def mon():
     framemono.pack( side = TOP )
     framevig.pack_forget()
     frameaff.pack_forget()
@@ -58,20 +48,8 @@ def hil():
     buttons.configure(command = hill)
     print "solve hill"
 
-def getvigenere():
-    labelp = Label(master, text="Phrase: ").grid(column=0, row=2, sticky="W")    
-    vphrase = Entry(master, width=25, textvariable=phrase)
-    vphrase.grid(row=3, column=0, columnspan=2)
-    cat.set("Key:")
-    catb.set("")
-    labelk = Label(master, textvariable=cat).grid(column=2, row=2, sticky="W")
-    vkey = Entry(master, width=16, textvariable=key)
-    vkey.grid(row=3, column=2, columnspan=2)
-    drbutton = Radiobutton(master, text="Decode", variable=v, value=0).grid(column=0, row=4, sticky="W")
-    erbutton = Radiobutton(master, text="Encode", variable=v, value=1).grid(column=1, row=4, sticky="W")
-
-    buttons = Button(master, text="Solve", command = vigenere)
-    buttons.grid(row=5, column=0)    
+def mono():
+    print "solve mono"
 
 def vigenere():
     del solnum[:]
@@ -98,24 +76,6 @@ def vigenere():
     print solution.get()
     print "solve vigenere"
 
-def getaffine():
-    labelp = Label(master, text="Phrase: ").grid(column=0, row=2, sticky="W")    
-    vphrase = Entry(master, width=25, textvariable=phrase)
-    vphrase.grid(row=3, column=0, columnspan=2)
-    cat.set("A: ")
-    labela = Label(master, textvariable=cat).grid(column=2, row=2, sticky="W")
-    va = Entry(master, width=7, textvariable=affinea)
-    va.grid(row=3, column=2)
-    catb.set("B: ")
-    labelb = Label(master, textvariable=catb).grid(column=3, row=2, sticky="W")
-    vb = Entry(master, width=7, textvariable=affineb)
-    vb.grid(row=3, column=3)
-    drbutton = Radiobutton(master, text="Decode", variable=v, value=0).grid(column=0, row=4, sticky="W")
-    erbutton = Radiobutton(master, text="Encode", variable=v, value=1).grid(column=1, row=4, sticky="W")
-
-    buttons = Button(master, text="Solve", command = affine)
-    buttons.grid(row=5, column=0)
-
 def affine():
     del solnum[:]
     del phrasenum[:]
@@ -139,84 +99,57 @@ def affine():
     print solution.get()
     print "solve affine"
 
-def gethill():
-    labelp = Label(master, text="Phrase: ").grid(column=0, row=2, sticky="W")    
-    vphrase = Entry(master, width=25, textvariable=phrase)
-    vphrase.grid(row=3, column=0, columnspan=2)
-    cat.set("Key:")
-    catb.set("")
-    labelk = Label(master, textvariable=cat).grid(column=2, row=2, sticky="W")
-    vkey = Entry(master, width=16, textvariable=key)
-    vkey.grid(row=3, column=2, columnspan=2)
-    drbutton = Radiobutton(master, text="Decode", variable=v, value=0).grid(column=0, row=4, sticky="W")
-    erbutton = Radiobutton(master, text="Encode", variable=v, value=1).grid(column=1, row=4, sticky="W")
-
-    buttons = Button(master, text="Solve", command = hill)
-    buttons.grid(row=5, column=0)
-
 def hill():
     print "solve hill"
 
 container = Frame(master)
 container.pack()
 
-label = Label(container, text="What type of cipher?")#.grid(column=0, row=0)
+label = Label(container, text="What type of cipher?")
 label.pack(side = TOP)
 
-buttonm = Button(container, text="Monoalphabetic", command = mono)
-#buttonm.grid(row=1, column=0)
+buttonm = Button(container, text="Monoalphabetic", command = mon)
 buttonm.pack(side = LEFT)
 
 buttonv = Button(container, text="Vigenere", command = vig)
-#buttonv.grid(row=1, column=1)
 buttonv.pack(side = LEFT)
 
 buttona = Button(container, text="Affine", command = aff)
-#buttona.grid(row=1, column=2)
 buttona.pack(side = LEFT)
 
 buttonh = Button(container, text="Hill", command = hil)
-#buttonh.grid(row=1, column=3)
 buttonh.pack(side = LEFT)
 
-"""for child in master.winfo_children():
-    child.grid_configure(padx=5, pady=5)"""
-
-#master.bind('<Return>', solve)
-
 framemono = Frame(master)
-labelp = Label(framemono, text="Phrase: ")#.grid(column=0, row=2, sticky="W")    
+labelp = Label(framemono, text="Phrase: ")   
 labelp.pack(side = LEFT)
 vphrase = Entry(framemono, width=25, textvariable=phrase)
 vphrase.pack(side = TOP)
-#vphrase.grid(row=3, column=0, columnspan=2)
 
 framevig = Frame(master)
-labelk = Label(framevig, text="Key:")#.grid(column=2, row=2, sticky="W")
+labelk = Label(framevig, text="Key:")
 labelk.pack(side = LEFT)
 vkey = Entry(framevig, width=16, textvariable=key)
 vkey.pack(side = LEFT)
 
 frameaff = Frame(master)
-labela = Label(frameaff, text="A: ")#.grid(column=2, row=2, sticky="W")
+labela = Label(frameaff, text="A: ")
 labela.pack(side = LEFT)
 va = Entry(frameaff, width=7, textvariable=affinea)
 va.pack(side = LEFT)
-labelb = Label(frameaff, text="B: ")#.grid(column=3, row=2, sticky="W")
+labelb = Label(frameaff, text="B: ")
 labelb.pack(side = LEFT)
 vb = Entry(frameaff, width=7, textvariable=affineb)
 vb.pack(side = LEFT)
 
 frameradio = Frame(master)
 frameradio.pack(side = BOTTOM)
-drbutton = Radiobutton(frameradio, text="Decode", variable=v, value=0)#.grid(column=0, row=4, sticky="W")
-erbutton = Radiobutton(frameradio, text="Encode", variable=v, value=1)#.grid(column=1, row=4, sticky="W")
+drbutton = Radiobutton(frameradio, text="Decode", variable=v, value=0)
+erbutton = Radiobutton(frameradio, text="Encode", variable=v, value=1)
 drbutton.pack(side = LEFT)
 erbutton.pack(side = LEFT)
 
 buttons = Button(frameradio, text="Solve", command = mono)
 buttons.pack(side = BOTTOM)
-#buttons.grid(row=5, column=0)
 
 master.mainloop()
-#uphrase = raw_input("which type")
